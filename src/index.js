0265da00fe42de6bb2534891f48872e0978e3467
IlyaZuev90 <god_of_iwowi@mail.ru>
2020-10-04 01:28:01 +0400

    init: complete task out-of-date

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length !== 0) {
+    const minimum = Math.min.apply(null, array);
+    return minimum;
+  } 
+  else {
+    return 0;
+  }
+       
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length !== 0) {
+    const maximum = Math.max.apply(null, array);
+    return maximum;
+  } 
+  else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length !== 0) {
+    var sum = 0;
+    for (var i = 0, j = array.length; i < j; i++) {
+      sum += array[i];
+  }
+  return sum / array.length;
+    
+  } 
+  else {
+    return 0;
+  }
 }
+
+
